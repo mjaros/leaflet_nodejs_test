fcf1d0ce15a9124f0807be1b543582e15a33f805
Matthias Jaros <mjaros@mjaros.com>
2013-03-07 15:09:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Dependencies
+ */
+
+var path = require('path');
+var fs = require('fs');
+var connect = require('connect');
+var http = require('http');
+
+/**
+ * Environment
+ */
+
+if (typeof process.env.NODE_ENV == 'undefined') {
+  process.env.NODE_ENV = 'development';
+}
+var env = process.env.NODE_ENV;
+
+/**
+ * Logging
+ */
+
+function log(message) {
+  var message = '['+new Date().toUTCString()+'] ' + message;
+  fs.appendFile(__dirname + '/log/' + env + '.log', message + '\n', function(err) {});
+  console.log(message);
+}
+
+/**
+ * HTTP server
+ */
+
+function startHTTPServer() {
+  var httpLogFile = fs.createWriteStream(__dirname + '/log/http_server.log', {flags: 'a'});
+  var app = connect()
+    .use(connect.logger({stream: httpLogFile}))
+    .use(connect.static('public'));
+  httpServer = http.createServer(app).listen(8000);
+  log('HTTP Server started');
+}
+
+startHTTPServer();
---
+++ public/app.js
@@ -0,0 +1,18 @@
+var osmAttribution = 'Map-Data <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-By-SA</a> by <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors';
+var osmUrl = 'http://{s}.tiles.vesseltracker.com/vesseltracker/{z}/{x}/{y}.png';
+var osmLayer = new L.tileLayer(osmUrl, {attribution: osmAttribution});
+
+var map = L.map(
+  'map',
+  {
+    closePopupOnClick: true,
+    markerZoomAnimation: false,
+    zoomAnimation: false,
+    worldCopyJump: true,
+    maxZoom: 17,
+    minZoom: 10
+  }
+);
+
+map.addLayer(osmLayer);
+map.setView([52.410,4.790], 14);
